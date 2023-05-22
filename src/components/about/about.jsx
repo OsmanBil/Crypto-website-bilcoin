a68355e57857aa71f8067c47a7f0e030a8eeb4e5
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-05-22 08:42:25 +0200

    index update - start about

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-
+import "./about.css"
 
 
 class About extends Component {
-    state = {  } 
-    render() { 
-        return <div> About</div>;
+    state = {}
+    render() {
+        return <div className='about-container'>
+            <div className='about-left'>
+                <div className='text-light'> <h1 className='display-1'> Bilcoin</h1> </div>
+                <div className='text-light'>Cryptro is a decentralised public blockchain and cryptocurrency
+                    project and is fully open source. Cryptro is developing
+                    a smart contract platform</div>
+                <div className='text-light'>Learn more - white paper</div>
+            </div>
+            <div className='about-right'>
+
+                <div className="card text-center mb-3" style="width: 18rem;">
+                    <div className="card-body">
+                        <h5 className="card-title">Special title treatment</h5>
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>;
     }
 }
- 
+
 export default About;
